4ef142897ca0001efa3caeba041d5270bff04f67
Scott Pullen <s.pullen05@gmail.com>
2014-06-23 21:28:48 -0400

    adding first_name and last_name attributes

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,24 @@ class User < ActiveRecord::Base
   has_many :organizations, through: :organization_admins
   has_many :donations
 
+  validates :first_name, presence: true
+  validates :last_name, presence: true
   validates :email, presence: true, 
-                    uniqueness: true,
+                    uniqueness: { if: :check_email_uniqueness },
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
-  validates :password, presence: true, length: { minimum: 8 }
-  validates :password_confirmation, presence: true
+  validates :password, presence: true, 
+                       length: { minimum: 8 }, 
+                       confirmation: true,
+                       if: :check_password_requirements
+  validates :password_confirmation, presence: true, if: :check_password_requirements
+
+private
+
+  def check_email_uniqueness
+    new_record? || email_changed?
+  end
+
+  def check_password_requirements
+    new_record? || !password.blank?
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140622221318) do
+ActiveRecord::Schema.define(version: 20140624011540) do
 
   create_table "bank_accounts", force: true do |t|
     t.string   "nickname"
@@ -82,6 +82,8 @@ ActiveRecord::Schema.define(version: 20140622221318) do
     t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
