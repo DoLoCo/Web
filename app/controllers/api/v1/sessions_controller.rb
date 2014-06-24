44a2c5601610e5e1971be7e83d50b62e7a378ca9
Scott Pullen <s.pullen05@gmail.com>
2014-06-23 21:35:37 -0400

    adding user serializer, cleaning up auth end point

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -4,14 +4,13 @@ module Api
       before_action :authenticate, only: [:test]
 
       def create
-        if params[:email] && params[:password]
-          user = User.find_by(email: params[:email].downcase)
-          if user && user.authenticate(params[:password])
-            token = AuthToken.issue_token({ user_id: user.id })
-            render json: { user: user, token: token }
-          else
-            render json: { error: "Invalid email/password combination" }, status: :unauthorized
-          end
+        user = User.find_by(email: params[:email].downcase) if !params[:email].blank?
+        if user && !params[:password].blank? && user.authenticate(params[:password])
+          token = AuthToken.issue_token({ user_id: user.id })
+          render json: { 
+            user: UserSerializer.new(user).to_json,
+            token: token 
+          }
         else
           render json: { error: "Invalid email/password combination" }, status: :unauthorized
         end
