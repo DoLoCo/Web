8668c9f59daae03ea7338a54e7ff15bd9fc37ba2
Scott Pullen <s.pullen05@gmail.com>
2014-06-30 20:08:18 -0400

    adding an exception handler for records not found

--- app/controllers/api/application_controller.rb
+++ app/controllers/api/application_controller.rb
@@ -1,13 +1,19 @@
 module Api
   class ApplicationController < ActionController::Base
     include Pundit
+
     rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+    rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
     protect_from_forgery with: :null_session
 
     respond_to :json
 
   protected
+
+    def current_user
+      @current_user ||= User.find(@token.first['user_id']) if @token
+    end
 
     def authenticate
       authenticate_token || render_unauthorized
@@ -24,12 +30,12 @@ module Api
       render json: { error: 'Authorization header not valid' }, status: :unauthorized
     end
 
-    def current_user
-      @current_user ||= User.find(@token.first['user_id']) if @token
-    end
-
     def user_not_authorized
       render json: { error: 'User not authorized to perform action' }, status: :unauthorized
+    end
+
+    def record_not_found
+      render json: { error: 'Record not found' }, status: :not_found
     end
     
   end
