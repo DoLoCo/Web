bf2562c3dadd9c8ebd993ba17d180c8ea45dacb2
Scott Pullen <s.pullen05@gmail.com>
2014-07-07 19:22:51 -0400

    filtering bank information from logs

--- config/application.rb
+++ config/application.rb
@@ -27,5 +27,7 @@ module Doloco
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    config.filter_parameters += [:password, :password_confirmation, :account_number, :routing_number, :account_type, :bank_name]
   end
 end
