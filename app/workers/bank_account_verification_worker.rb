9038f960a5cf010769d8e4cf2717fa4970e6b0ec
Scott Pullen <s.pullen05@gmail.com>
2014-08-13 19:47:24 -0400

    catching failed verification requests

--- app/workers/bank_account_verification_worker.rb
+++ app/workers/bank_account_verification_worker.rb
@@ -5,12 +5,12 @@ class BankAccountVerificationWorker
     bank_account = BankAccount.find(bank_account_id)
     
     if bank_account.unverified?
-      gateway_ba = Balanced::BankAccount.fetch("/bank_accounts/#{bank_account.gateway_reference_id}")  
-      verification = gateway_ba.verify
+      begin
+        gateway_ba = Balanced::BankAccount.fetch("/bank_accounts/#{bank_account.gateway_reference_id}") 
+        gateway_ba.verify
 
-      if verification && verification['bank_account_verifications'][0]['verification_status'] == 'pending'
         bank_account.verification_created!
-      else
+      rescue Balanced::Error => e
         self.perform_async(bank_account_id)
       end
     end
