a844951a613486dee571f0b5dea7ec4fbcc07256
Scott Pullen <s.pullen05@gmail.com>
2014-08-13 19:36:59 -0400

    handling failed bank account creation

--- app/services/bank_account_create.rb
+++ app/services/bank_account_create.rb
@@ -14,18 +14,27 @@ class BankAccountCreate
       @bank_account.last_four = @bank_account.account_number.split(//).last(4).join
     end
 
-    gateway_bank_account = Balanced::BankAccount.new(
-      account_number: @bank_account.account_number,
-      account_type: @bank_account.account_type,
-      routing_number: @bank_account.routing_number,
-      name: @ownable.name
-    ).save
+    begin
+      gateway_bank_account = Balanced::BankAccount.new(
+        account_number: @bank_account.account_number,
+        account_type: @bank_account.account_type,
+        routing_number: @bank_account.routing_number,
+        name: @ownable.name
+      ).save
 
-    # TODO: handle errors, didn't show up in the documentation need to dig around
-    @bank_account.gateway_reference_id = gateway_bank_account['bank_accounts'][0]['id']
+      @bank_account.gateway_reference_id = gateway_bank_account.id
 
-    if @bank_account.save
-      BankAccountVerificationWorker.perform_async(@bank_account.id)
+      if @bank_account.save
+        BankAccountVerificationWorker.perform_async(@bank_account.id)
+      end
+
+    rescue Balanced::BadRequest => ex
+      @bank_account.valid? # force validations
+
+      unless ex.error_message.nil?
+        error = ex.body[:errors][0]['extras']
+        @bank_account.errors.add(error.keys.first, error.values.first)
+      end
     end
 
     @bank_account
