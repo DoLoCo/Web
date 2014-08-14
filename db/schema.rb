bd5a7bb580d0b3457518737832fc6157a801f427
Scott Pullen <s.pullen05@gmail.com>
2014-08-14 19:23:50 -0400

    addin status column to migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140708234637) do
+ActiveRecord::Schema.define(version: 20140814232259) do
 
   create_table "bank_accounts", force: true do |t|
     t.string   "bank_account_name"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 20140708234637) do
     t.integer  "amount"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "status"
   end
 
   add_index "donations", ["bank_account_id"], name: "index_donations_on_bank_account_id", using: :btree
