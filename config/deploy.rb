fa8a96c1cf42a53f236d220921898432a4f696fd
Scott Pullen <s.pullen05@gmail.com>
2014-08-17 12:25:41 -0400

    updating rails version and adding some more deploy stuffz

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,46 +1,36 @@
 # config valid only for Capistrano 3.1
 lock '3.2.1'
 
-set :application, 'my_app_name'
+set :ssh_options, {
+  user: 'deploy'
+}
+
+set :scm, :git
+
+set :application, 'DoLoCo'
 set :repo_url, 'git@example.com:DoLoCo/Web.git'
 
-# Default branch is :master
-# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
+set :stages, ['production']
 
-# Default deploy_to directory is /var/www/my_app
-# set :deploy_to, '/var/www/my_app'
+set :deploy_to, '/home/deploy/www'
 
-# Default value for :scm is :git
-# set :scm, :git
 
-# Default value for :format is :pretty
-# set :format, :pretty
-
-# Default value for :log_level is :debug
-# set :log_level, :debug
-
-# Default value for :pty is false
+set :format, :pretty
 set :pty, true
 
-# Default value for :linked_files is []
 set :linked_files, %w{config/database.yml}
-
-# Default value for linked_dirs is []
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for keep_releases is 5
 set :keep_releases, 5
+
+set :rvm_ruby_version, 'ruby-2.1.2@doloco'
 
 namespace :deploy do
 
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
-      # Your restart mechanism here, for example:
-      # execute :touch, release_path.join('tmp/restart.txt')
+      execute :touch, release_path.join('tmp/restart.txt')
     end
   end
 
