8703aeceb3d6f783b81b5c347711009c786e255b
Scott Pullen <s.pullen05@gmail.com>
2014-09-06 11:42:47 -0400

    fixing the bank account policy

--- app/policies/bank_account_policy.rb
+++ app/policies/bank_account_policy.rb
@@ -14,9 +14,9 @@ class BankAccountPolicy < Struct.new(:user, :bank_account)
 private
   
   def is_user_permitted?
-    if BankAccount::OWNABLE_TYPES[:user]
+    if bank_account.ownable_type == BankAccount::OWNABLE_TYPES[:user]
       ownable.id == user.id
-    elsif BankAccount::OWNABLE_TYPES[:organization]
+    elsif bank_account.ownable_type == BankAccount::OWNABLE_TYPES[:organization]
       bank_account.ownable.is_admin?(user.id)
     end
   end
