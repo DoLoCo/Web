edf62ec2b99e9d193a278ac3fb5d0f607dd61847
Scott Pullen <s.pullen05@gmail.com>
2014-09-10 21:11:24 -0400

    re-org of how the balanced api key is initialized

--- config/initializers/balanced_payments.rb
+++ config/initializers/balanced_payments.rb
@@ -1,5 +1,1 @@
-if Rails.env.production?
-  Balanced.configure(ENV['BALANCED_PAYMENTS_MARKETPLACE_KEY'])
-else
-  Balanced.configure('ak-test-2q80HU8DISm2atgm0iRKRVIePzDb34qYp')
-end
+Balanced.configure(Rails.application.secrets.balanced_api_key)
