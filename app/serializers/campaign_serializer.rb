86f7c2bd537e587ae9899ae8232b0c66b7986b9b
Bill Pullen <bill.pull@gmail.com>
2014-09-12 18:54:19 -0400

    Send organization along with campaigns

--- app/serializers/campaign_serializer.rb
+++ app/serializers/campaign_serializer.rb
@@ -1,6 +1,8 @@
 class CampaignSerializer < ActiveModel::Serializer
   attributes :id, :organization_id, :bank_account_id, :title, :description, :status, :target_amount, :target_date, :donations_amount_sum
 
+  has_one :organization
+
   # TODO: add bank_account_id if admin of campaign's organization
 =begin
   def filter(keys)
