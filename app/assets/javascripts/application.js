6b22f941e90d598ceb591a751b2b84a81f95be36
Bill Pullen <bill.pull@gmail.com>
2014-09-16 21:55:57 -0400

    webview js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,56 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+function getUrlParameter(sParam)
+{
+    var sPageURL = window.location.search.substring(1);
+    var sURLVariables = sPageURL.split('&');
+    for (var i = 0; i < sURLVariables.length; i++) 
+    {
+        var sParameterName = sURLVariables[i].split('=');
+        if (sParameterName[0] == sParam) 
+        {
+            return sParameterName[1];
+        }
+    }
+}   
+
+function handleBalancedResponse(response) {
+  if (response.status_code === 201) {
+    var fundingInstrument = response.cards != null ? response.cards[0] : response.bank_accounts[0];
+
+    $.ajax({
+      type: 'POST',
+      url: '/api/v1/bank_accounts',
+      data: {
+	bank_account: {
+		instrument_href: fundingInstrument.href
+        }
+      },
+      beforeSend: function (request) {
+         var token = getUrlParameter('token');
+         request.setRequestHeader('Authorization', 'Token token="'+ token +'"');
+      }
+    });
+    
+  } else {
+    window.alert("Bank Account Failed to Add");
+  }
+}
 
 $(function() {
   balanced.init();
+
+  $(document).on('click', '#ba-create', function (e){
+      e.preventDefault();
+
+  var payload = {
+    name: $('#ba-name').val(),
+    routing_number: $('#ba-routing').val(),
+    account_number: $('#ba-number').val()
+  };
+
+  // Create bank account
+  balanced.bankAccount.create(payload, handleBalancedResponse);
+  });
 });
