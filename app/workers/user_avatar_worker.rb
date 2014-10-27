696679a18f9c97fbd695e3a6d7e0a8b6610535cb
Bill Pullen <bill.pull@gmail.com>
2014-10-26 22:56:08 -0400

    Update user_avatar_worker.rb

--- app/workers/user_avatar_worker.rb
+++ app/workers/user_avatar_worker.rb
@@ -4,12 +4,11 @@ class UserAvatarWorker
   include Sidekiq::Worker
 
   def perform(user_id)
-  	user = User.find(user_id);
-  	gravatar_prefix = "http://www.gravatar.com/avatar/";
+  	user = User.find(user_id)
+  	gravatar_prefix = "http://www.gravatar.com/avatar/"
   	# https://en.gravatar.com/site/implement/images/
-  	email_hash = Digest::MD5.hexdigest(user.email.gsub(/\s+/, "").downcase);
-  	gravatar_url = gravatar_prefix + email_hash;
-  	user.update(image_url: gravatar_url);
-	user.save;
+  	email_hash = Digest::MD5.hexdigest(user.email.gsub(/\s+/, "").downcase)
+  	gravatar_url = gravatar_prefix + email_hash
+  	user.update(image_url: gravatar_url)
   end
 end
